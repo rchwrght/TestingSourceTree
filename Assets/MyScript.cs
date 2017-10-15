17b82d33a6d3e4eaf29390acf7760b1fcd5b849b
Rich Wright <rchwrght@gmail.com>
2017-10-15 14:03:38 +0200

    Initial Commit on Source Tree

---
+++ Assets/MyScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        print("VCS test");
+        print("Added extra Line");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
